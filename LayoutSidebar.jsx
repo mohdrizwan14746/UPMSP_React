0ec76fba621fbfe71b8ed329ccd0c7fad7ba1cff
mohdrizwan14746 <mohdrizwan14746@gmail.com>
2024-09-11 19:20:28 +0530

    Add files via upload

---
+++ LayoutSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import BasicLayout from '../pages/BasicLayout'
+import StudentSidebar from './StudentSidebar'
+
+const LayoutSidebar = () => {
+  return (
+    <BasicLayout>
+        <StudentSidebar/>
+        </BasicLayout>
+       
+    
+  )
+}
+
+export default LayoutSidebar
